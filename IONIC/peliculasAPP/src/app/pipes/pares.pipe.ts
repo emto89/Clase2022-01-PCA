d9fe4fa9e037a548d849e34f108d14333dd33038
CITYLENDINGINC\mdiaz <mdiaz@citylendinginc.com>
2022-02-09 17:41:56 -0500

    Se envia clase de 8 de febrero

--- IONIC/peliculasAPP/src/app/pipes/pares.pipe.ts
+++ IONIC/peliculasAPP/src/app/pipes/pares.pipe.ts
@@ -5,13 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ParesPipe implements PipeTransform {
 
-  transform(arr: any[]): any[] {
-    const pares = arr.reduce( (result:any[],value,index,array)=>{
-      if(index % 2 === 0){
-        result.push( array.slice( index, index+2 ));
+  transform( arr: any[] ): any[] {
+
+    const pares = arr.reduce( (result, value, index, array) => {
+
+      if ( index % 2 === 0) {
+        result.push(array.slice(index, index + 2));
       }
+      return result;
     }, []);
+
     return pares;
-  }
+ }
 
 }
